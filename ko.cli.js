a15f0f72f8987254efb217cf996fc9be2a464c35
nik frank <nikbfrank@gmail.com>
2016-01-16 23:43:16 +0200

    almost done component maker

--- ko.cli.js
+++ ko.cli.js
@@ -1,5 +1,43 @@
 #!/usr/bin/env node
+'use strict';
 
 require('shelljs/global');
+let fs = require('fs');
+require.extensions['.tpl'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
 
-console.log(which('git'));
+let args = process.argv.slice(2);
+let pkg, files, cons;
+try{
+    pkg = require('./'+args[0]+'/package.json');
+    files = pkg.files;
+    cons = pkg.constants;
+
+    mkdir('-p', args[1]);
+}catch(e){
+    console.error(e, 'no such ko command as '+args[0]);
+    exit(1);
+}
+
+for(let i=files.length; i-->0;){
+    
+    // need to replace constants in all file-names and file-contents
+
+    let filename = files[i];
+    let blob = require('./'+args[0]+'/'+files[i]);
+
+console.log(cons, filename);
+
+    for(let con in cons){
+        filename = filename.replace(con, cons[con]);
+        blob = blob.replace(con, cons[con]);
+    }
+
+console.log(filename);
+
+    blob.to(args[1]+'/'+filename);
+
+}
+
+console.log(ls());
